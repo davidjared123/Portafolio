65934f41ed1fda03e741a6427d1eb49df44134fa
David Perez <davidjared123@gmail.com>
2020-11-13 12:32:21 -0400

    contacto y footer

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+const typed = new Typed('.typed', {
+    strings: ['Hola Mundo', 'Me llamo David'],
+    typeSpeed: 75,
+    startDelay: 300,
+    loop: true,
+    loopCount: false,
+    backSpeed: 75,
+
+})
