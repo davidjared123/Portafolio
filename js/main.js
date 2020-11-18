a2f51d6801eb2c95e0e234288f53d40ac615079d
David Perez <davidjared123@gmail.com>
2020-11-18 13:22:17 -0400

    Activar display menu

--- js/main.js
+++ js/main.js
@@ -6,5 +6,18 @@ const typed = new Typed('.typed', {
     loop: true,
     loopCount: false,
     backSpeed: 75,
-
 })
+
+function menu() {
+    const label = document.querySelector('#check')
+
+    if (label.checked) {
+        // console.log('activo')
+        document.querySelector('.nav-links').style.display = 'block';
+    } else {
+        // console.log('desactivado')
+        document.querySelector('.nav-links').style.display = 'none';
+    }
+}
+
+menu()
